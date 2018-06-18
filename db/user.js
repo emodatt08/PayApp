c189990c0e711aee240deef9a39ee728d804ed61
emodatt08 <emodatt08@gmail.com>
2018-06-18 19:59:26 +0000

    added mongoose connection

---
+++ db/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema;
+
+var userSchema = new schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+
+    email:{
+        type:String,
+        required:true,
+    },
+})
+
+module.exports = mongoose.model('users', userSchema, 'users');
